d34ec873708d5e26685004e95742aa7ad0aa9f44
Kerter0507 <ivan.rodyakin@yandex.ru>
2026-04-29 16:13:06 +0300

    Update randommops.py

--- randommops.py
+++ randommops.py
@@ -8,7 +8,8 @@ from telegram.ext import ( ApplicationBuilder, CommandHandler, ContextTypes, Cal
 from telegram.ext import MessageHandler, filters
 TOKEN = ("8788193406:AAExA1aI4feuzuf5hgD2CPc5r9MsCEL0hT4")
 ADMIN_ID = 1257248239
-COOLDOWN = 1800
+ADMIN_ID = 1257248239
+COOLDOWN = 10800
 
 IMAGES = [
     ("https://i.pinimg.com/originals/82/b9/8f/82b98f58172f08476b66d6fa59f31b3f.jpg?nii=t",
@@ -128,12 +129,6 @@ async def shop(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def mops(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # универсальное сообщение (группа / лс / кнопка)
-    if update.message:
-        message = update.message
-    else:
-        message = update.callback_query.message
-
     user_id = update.effective_user.id
     now = time.time()
     today = datetime.date.today()
@@ -144,8 +139,6 @@ async def mops(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if row:
         last_used, streak, coins = row
         last_date = datetime.date.fromtimestamp(last_used)
-
-        # КД — теперь общий (и для группы, и для лс)
         if now - last_used < COOLDOWN:
             remaining = int(COOLDOWN - (now - last_used))
 
@@ -153,9 +146,10 @@ async def mops(update: Update, context: ContextTypes.DEFAULT_TYPE):
             minutes = (remaining % 3600) // 60
 
             if hours > 0:
-                await message.reply_text(f"⏳ Подожди {hours} ч. {minutes} мин.")
+                await update.message.reply_text(f"⏳ Подожди {hours} ч. {minutes} мин.")
             else:
-                await message.reply_text(f"⏳ Подожди {minutes} мин.")
+                await update.message.reply_text(f"⏳ Подожди {minutes} мин.")
+
             return
 
         if last_date == today:
@@ -171,36 +165,24 @@ async def mops(update: Update, context: ContextTypes.DEFAULT_TYPE):
     cursor.execute("SELECT card_id FROM collection WHERE user_id=?", (user_id,))
     owned = {row[0] for row in cursor.fetchall()}
 
+    # список доступных (которых еще нет)
     available = [i for i in range(len(IMAGES) - 1) if i not in owned]
-
     if not available:
-        await message.reply_text("Ты собрал все карты 🐶")
+        await query.message.reply_text("Ты собрал все карты 🐶")
         return
 
     card_id = random.choice(available)
 
+    # если все собраны — даем любую
+    available = [i for i in range(len(IMAGES) - 1) if i not in owned]
+
+    if not available:
+        card_id = random.randint(0, len(IMAGES) - 2)
+    else:
+        card_id = random.choice(available)
+
     img, cap = IMAGES[card_id]
 
-    rarity = get_rarity(cap)
-    reward = REWARD.get(rarity, 5)
-    coins += reward
-
-    cursor.execute("INSERT OR IGNORE INTO collection VALUES (?, ?)", (user_id, card_id))
-    cursor.execute("""
-    INSERT INTO users (user_id, last_used, streak, coins)
-    VALUES (?, ?, ?, ?)
-    ON CONFLICT(user_id) DO UPDATE SET
-        last_used=excluded.last_used,
-        streak=excluded.streak,
-        coins=excluded.coins
-    """, (user_id, now, streak, coins))
-
-    conn.commit()
-
-    await message.reply_photo(
-        photo=img,
-        caption=f"{cap}\n\n💰 +{reward}\n🔥 {streak}\n💸 {coins}"
-    )
     rarity = get_rarity(cap)
     reward = REWARD.get(rarity, 5)
     coins += reward
